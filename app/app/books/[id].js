553b883e2db76323e3307a46c642ae7948f079bb
charmhou85 <charmhou85@gmail.com>
2026-03-26 20:21:53 +0800

    add json

---
+++ app/app/books/[id].js
@@ -0,0 +1,5 @@
+import { View, Text, StyleSheet, StatusBar, Image, TouchableOpacity } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+
+import PB from '../../json/PB.json';
+import NB from '../../json/NB.json';
